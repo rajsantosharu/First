3878d7df0baf0f3a112dcb267c2932aa5e48054a
santosharuraj <santoshraj90068@gmail.com>
2023-01-14 22:50:59 +0530

    new change

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -3,6 +3,8 @@ import "./Card.css";
 import { useContext } from "react";
 import { themeContext } from "../../Context";
 // some changes again111111111
+// sdaadasfasfasfa
+//check kro
 const Card = ({ emoji, heading, details }) => {
 	const theme = useContext(themeContext);
 	const darkMode = theme.state.darkMode;
